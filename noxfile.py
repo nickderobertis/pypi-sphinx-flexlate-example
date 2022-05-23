a317d3f60f87345ed949dee51f78996df02ba26f
Nick DeRobertis <derobertis.nick@gmail.com>
2022-05-23 12:37:26 +0000

    Update flexlate templates
    copier-pypi-sphinx-flexlate: f0a27dba040497606f8e2e6a297c5e5a
    
    -------------------BEGIN FLEXLATE TRANSACTION-------------------
    {
      "type": "update",
      "target": null,
      "out_root": null,
      "data": [
        {
          "author_email": "derobertis.nick@gmail.com",
          "full_name": "Pypi Sphinx Flexlate Example",
          "google_analytics_id": "",
          "install_packages": "",
          "logo_url": "",
          "main_branch_name": "main",
          "package_author": "Nick DeRobertis",
          "package_directory": "pypi_sphinx_flexlate_example",
          "package_name": "pypi-sphinx-flexlate-example",
          "repo_name": "pypi-sphinx-flexlate-example",
          "repo_username": "nickderobertis",
          "short_description": "Example output for https://github.com/nickderobertis/copier-pypi-sphinx-flexlate"
        }
      ],
      "id": "deb745a1-8a70-4283-bf3d-8f7955e9be07"
    }

---
+++ noxfile.py
@@ -0,0 +1,44 @@
+import nox
+
+nox.options.sessions = ["format", "lint", "test"]
+
+@nox.session(python=False)
+def format(session):
+    if session.interactive:
+        # When run as user, format the files in place
+        session.run("isort", ".")
+        session.run("black", ".")
+    else:
+        # When run from CI, fail the check if formatting is not correct
+        session.run("isort", "--check-only", ".")
+        session.run("black", "--check", ".")
+
+
+@nox.session(python=False)
+def lint(session):
+    session.run(
+        "flake8", "--count", "--select=E9,F63,F7,F82", "--show-source", "--statistics"
+    )
+    session.run(
+        "flake8",
+        "--count",
+        "--exit-zero",
+        "--max-complexity=10",
+        "--max-line-length=127",
+        "--statistics",
+    )
+    session.run("mypy")
+
+
+@nox.session
+def test(session):
+    session.install("-r", "test-requirements.txt", "--upgrade", "--upgrade-strategy", "eager")
+    session.install(".")
+    session.run("pytest")
+
+
+@nox.session
+def test_coverage(session):
+    session.install("-r", "test-requirements.txt", "--upgrade", "--upgrade-strategy", "eager")
+    session.install(".")
+    session.run("pytest", "--cov=./", "--cov-report=xml")
