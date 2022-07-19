51992760a0f53998e7e115f31facafdd69a0297a
Nick DeRobertis <derobertis.nick@gmail.com>
2022-07-19 21:36:41 +0000

    Update flexlate templates
    copier-pypi-sphinx-flexlate: 34d8ec59cc9bf02e2eb2b6e5c7f62386
    
    -------------------BEGIN FLEXLATE TRANSACTION-------------------
    {
      "type": "update",
      "target": null,
      "out_root": null,
      "data": [
        {
          "author_email": "derobertis.nick@gmail.com",
          "full_name": "Pypi Sphinx Flexlate Example",
          "google_analytics_id": "",
          "install_packages": "",
          "logo_url": "",
          "main_branch_name": "main",
          "minimum_python_version": "3.8",
          "os_support": [
            "ubuntu",
            "macos",
            "windows"
          ],
          "package_author": "Nick DeRobertis",
          "package_directory": "pypi_sphinx_flexlate_example",
          "package_name": "pypi-sphinx-flexlate-example",
          "repo_name": "pypi-sphinx-flexlate-example",
          "repo_username": "nickderobertis",
          "short_description": "Example output for https://github.com/nickderobertis/copier-pypi-sphinx-flexlate",
          "topics": [
            "Flexlate",
            "PyPI",
            "Sphinx",
            "Multi-word Example"
          ]
        }
      ],
      "id": "014fed7a-9dde-4793-b2b4-4db694e73be9"
    }

--- noxfile.py
+++ noxfile.py
@@ -1,20 +1,8 @@
-import os
-import platform
-import shutil
-from pathlib import Path
-from typing import Literal
+import subprocess
 
 import nox
 
 nox.options.sessions = ["format", "strip", "lint", "test"]
-
-VENVS_DIR = Path("~").expanduser() / ".venvs"
-PROJECT_NAME = Path(__file__).parent.name
-PROJECT_VENV_DIR = VENVS_DIR / PROJECT_NAME
-LINT_VENV_DIR = PROJECT_VENV_DIR / "lint"
-LINT_VENV_DIR_STR = str(LINT_VENV_DIR)
-
-VenvName = Literal["lint"]
 
 
 @nox.session(python=False)
@@ -53,7 +41,8 @@ def lint(session):
     _setup_venv(session, "lint")
 
     def run_lint(*args):
-        _run_in_venv(session, "lint", *args)
+        args_str = " ".join(args)
+        _run(f"mvenv run lint -- {args_str}")
 
     run_lint(
         "flake8", "--count", "--select=E9,F63,F7,F82", "--show-source", "--statistics"
@@ -117,84 +106,36 @@ def docs(session):
         session.run("bash", "./dev-server.sh")
 
 
-def _setup_venv(session, venv_name: VenvName):
-    venv_path = _venv_path(venv_name)
+def _setup_venv(session, venv_name: str):
     if _venv_exists(venv_name):
-        print(f"Using existing {venv_name} venv at {venv_path}")
         return
-    session.run("virtualenv", str(venv_path))
-    _update_venv(session, venv_name)
+    session.run("mvenv", "sync", venv_name)
 
 
-def _venv_path(venv_name: VenvName) -> Path:
-    return PROJECT_VENV_DIR / venv_name
+def _venv_exists(venv_name: str):
+    exists_str = _run(
+        f"mvenv info {venv_name} -i json | jq '.[0].exists'", stream=False
+    ).strip()
+    return exists_str == "true"
 
 
-def _venv_exists(venv_name: VenvName):
-    venv_dir = _venv_path(venv_name)
-    return venv_dir.exists()
-
-
-@nox.session(python=False)
-def venv(session):
-    # First argument is one of "delete", "update", "create"
-    # Second argument is the name of the venv
-    if len(session.posargs) < 2:
-        raise ValueError(
-            f"Must supply first action (delete, update, create), then venv name, got {session.posargs}"
-        )
-    action = session.posargs[0]
-    venv_name = session.posargs[1]
-    if action == "delete":
-        _delete_venv(venv_name)
-    elif action == "update":
-        _update_venv(session, venv_name)
-    elif action == "create":
-        _setup_venv(session, venv_name)
-    else:
-        raise ValueError(f"Action must be delete or update, got {action}")
-
-
-def _run_in_venv(session, venv_name: VenvName, *args):
-    new_command = _venv_command(venv_name, *args)
-    session.run(*new_command)
-
-
-def _venv_command(venv_name: VenvName, *args):
-    if platform.system() == "Windows":
-        return _venv_command_windows(venv_name, *args)
-    return _venv_command_unix(venv_name, *args)
-
-
-def _venv_command_unix(venv_name: VenvName, *args):
-    venv_dir = _venv_path(venv_name)
-    venv_command = os.path.sep.join((str(venv_dir), "bin", args[0]))
-    new_args = [venv_command, *args[1:]]
-    return new_args
-
-
-def _venv_command_windows(venv_name: VenvName, *args):
-    venv_dir = _venv_path(venv_name)
-    venv_command = os.path.sep.join((str(venv_dir), "Scripts", args[0] + ".exe"))
-    new_args = [venv_command, *args[1:]]
-    return new_args
-
-
-def _delete_venv(venv_name: VenvName):
-    venv_dir = _venv_path(venv_name)
-    if venv_dir.exists():
-        shutil.rmtree(venv_dir)
-        print(f"Deleted venv {venv_name} from {venv_dir}")
-    else:
-        raise ValueError(f"Venv {venv_name} does not exist at {venv_dir}")
-
-
-def _update_venv(session, venv_name: VenvName):
-    venv_dir = _venv_path(venv_name)
-    if not venv_dir.exists():
-        raise ValueError(
-            f"Venv {venv_name} does not exist at {venv_dir}, cannot update"
-        )
-    _run_in_venv(
-        session, venv_name, "pip", "install", "-r", f"{venv_name}-requirements.txt"
+def _run(
+    command: str,
+    stream: bool = True,
+) -> str:
+    process = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        shell=True,
     )
+    buffer = b""
+    for c in iter(lambda: process.stdout.read(1), b""):  # type: ignore
+        if stream:
+            print(c.decode(), end="")
+        buffer += c
+    process.wait()
+    if process.returncode != 0:
+        raise subprocess.CalledProcessError(process.returncode, command)
+
+    return buffer.decode()
