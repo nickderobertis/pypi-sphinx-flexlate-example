90887449a20b7a8d2c2af9804220964261461cee
nickderobertis <nickderobertis@users.noreply.github.com>
2023-01-28 14:35:42 +0000

    Automated deployment: Sat Jan 28 14:35:42 UTC 2023 fe0ec855b65a4525345de65d08767c2a1ece49a7

---
+++ _downloads/73501fa333d1f6ffd0ee7abb1a0e0cc7/simple_example.py
@@ -0,0 +1,35 @@
+"""
+Example Notebook
+================
+
+This is an example notebook to show how notebook examples in the docs
+look.
+
+"""
+
+print("woo")
+
+from IPython.display import display, HTML
+display(HTML('<img src="https://http.cat/200"></img>'))
+
+
+######################################################################
+# Heading 2
+# ---------
+# 
+
+
+######################################################################
+# Heading 3
+# ~~~~~~~~~
+# 
+# And a paragraph, then an equation.
+# 
+
+
+######################################################################
+# .. math::
+# 
+# 
+#    \sum_i^n {a_i b_i}
+# 
